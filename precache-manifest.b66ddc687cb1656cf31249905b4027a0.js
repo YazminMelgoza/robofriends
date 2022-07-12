77d0c74bbdb9d0cd5d3e0b81a963e88f551bed93
YazminMelgoza <yazminmelgozazamarripa@gmail.com>
2022-07-12 10:54:59 -0500

    Updates

---
+++ precache-manifest.b66ddc687cb1656cf31249905b4027a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55632ae06a6e62d1d75a",
+    "url": "/robofriends/static/css/main.a193e999.chunk.css"
+  },
+  {
+    "revision": "55632ae06a6e62d1d75a",
+    "url": "/robofriends/static/js/main.ed2a62e3.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "9dd88bacfc061531004c",
+    "url": "/robofriends/static/css/2.d7b5d722.chunk.css"
+  },
+  {
+    "revision": "9dd88bacfc061531004c",
+    "url": "/robofriends/static/js/2.cf8dcc85.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "b09fb28625bdb43a6b7c935569a1c24a",
+    "url": "/robofriends/index.html"
+  }
+];
